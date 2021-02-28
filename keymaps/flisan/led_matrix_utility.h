5e121829c888c60d7d2c7b8e11ebd88ad0144486
loxygenk <loxygen.k@gmail.com>
2021-02-28 14:08:48 +0900

    🏷️ Create "Color" struct

--- keymaps/flisan/led_matrix_utility.h
+++ keymaps/flisan/led_matrix_utility.h
@@ -4,7 +4,6 @@
 
 #define CLAMP(value, min, max) (value > max ? max : (value < min ? min : value))
 #define RGB_CLAMP(value) CLAMP(value, 0, 255)
-#define RGB_GRAY(level) RGB_CLAMP(level), RGB_CLAMP(level), RGB_CLAMP(level)
 
 void set_rgb(uint8_t led_min, uint8_t led_max, uint8_t red, uint8_t green, uint8_t blue) {
   for (uint8_t i = led_min; i < led_max; i++) {
